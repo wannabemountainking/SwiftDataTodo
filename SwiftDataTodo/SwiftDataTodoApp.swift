f4b93f706c1eb3a7df119f36f51a1208cc359415
wannabemountainking <kdy0096@gmail.com>
2026-03-12 16:04:04 +0900

    Initial Commit

---
+++ SwiftDataTodo/SwiftDataTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTodoApp.swift
+//  SwiftDataTodo
+//
+//  Created by YoonieMac on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
