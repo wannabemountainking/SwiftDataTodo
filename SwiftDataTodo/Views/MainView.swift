f7aa8a41507562fda182bedfcb9315bf63146a14
김도윤 <kdy0096@gmail.com>
2026-03-15 03:31:32 +0900

    MainView

--- SwiftDataTodo/Views/MainView.swift
+++ SwiftDataTodo/Views/MainView.swift
@@ -15,7 +15,7 @@ struct MainView: View {
     @Query private var tasks: [TaskModel]
     @State private var showAlert: Bool = false
     @State private var taskTodoDelete: TaskModel?
-    @State private var isNewTask: Bool = true
+    @State private var isNewTask: Bool = false
     @State private var sortOrder: SortOrder = .forward
     @State private var filterOn: Bool = false
     
@@ -36,14 +36,105 @@ struct MainView: View {
                         description:
                             Text("☝️ 위의 추가 버튼을 클릭해서 해야할 일을 작성하세요.")
                             .font(.title3)
-                    )
+                    )//: ContentUnavailableView
+                } else {
+                    List {
+                        ForEach(sortedAndFilteredTodos) { task in
+                            NavigationLink {
+                                //destination
+                                CreateUpdateTaskView(task: task)
+                            } label: {
+                                TaskRowView(task: task)
+                                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                        Button {
+                                            // action
+                                            taskTodoDelete = task
+                                            showAlert = true
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                                .font(.headline)
+                                        }
+                                        .tint(Color.red.opacity(0.8))
+                                    }//: swipeAction: trailing (Delete)
+                                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                                        Button {
+                                            // action
+                                            task.isComplete.toggle()
+                                        } label: {
+                                            Label(
+                                                task.isComplete ? "Complete" : "OnGoing",
+                                                systemImage: task.isComplete ? "arrow.uturn.backward.square" : "checkmark.square"
+                                            )
+                                            .font(.headline)
+                                        }
+                                        .tint(task.isComplete ? .green.opacity(0.8) : .blue.opacity(0.8))
+                                    }//: .swipeAction: .leading (isImportant)
+                            } //:NavLink
+                        } //:LOOP
+                    } //:LIST
+                } //:CONDITION
+            } //:VSTACK
+            .navigationTitle(tasks.isEmpty ? "할 일 입력" : "할 일 목록")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        //action
+                        sortOrder = (sortOrder == .forward) ? .reverse : .forward
+                    } label: {
+                        Image(systemName: "arrow.up.arrow.down.circle")
+                            .font(.title2)
+                            .symbolVariant(sortOrder == .forward ? .fill : .none)
+                            .foregroundStyle(sortOrder == .forward ? Color.green : Color.red)
+                    }
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        //action
+                        filterOn.toggle()
+                    } label: {
+                        Image(systemName: "exclamationmark.triangle")
+                            .font(.title2)
+                            .symbolVariant(filterOn ? .fill : .none)
+                            .foregroundStyle(filterOn ? Color.red : Color.gray)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        //action
+                        isNewTask.toggle()
+                    } label: {
+                        Image(systemName: "plus.app.fill")
+                            .font(.title2)
+                            .foregroundStyle(Color.blue.opacity(0.7))
+                    }
                 }
             }
-            .navigationTitle(tasks.isEmpty ? "할 일 입력" : "할 일 목록")
+            .sheet(isPresented: $isNewTask) {
+                //dismiss
+            } content: {
+                NavigationStack {
+                    CreateUpdateTaskView()
+                }
+            }
+            .alert(
+                "정말로 삭제하시겠습니까?",
+                isPresented: $showAlert) {
+                    Button("Delete", role: .destructive) {
+                        guard let taskTodoDelete else {return}
+                        modelContext.delete(taskTodoDelete)
+                    }
+                } message: {
+                    Text("선택하신 할 일을 삭제하시겠습니까?")
+                }
         } //:NAVIGATION
     }//:body
 }
 
-#Preview {
+#Preview("Task 있음") {
+    MainView()
+        .modelContainer(TaskModel.previewTodo)
+}
+
+#Preview("Task 없음") {
     MainView()
 }
