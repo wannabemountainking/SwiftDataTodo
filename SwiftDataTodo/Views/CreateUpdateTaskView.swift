0f3eb46bb9e31dd95cbe9fa88a31ba32cee9967a
wannabemountainking <kdy0096@gmail.com>
2026-03-14 23:00:53 +0900

    CreateUpdateView

--- SwiftDataTodo/Views/CreateUpdateTaskView.swift
+++ SwiftDataTodo/Views/CreateUpdateTaskView.swift
@@ -11,8 +11,8 @@ import SwiftData
 struct CreateUpdateTaskView: View {
     
     // MARK: - Properties
-    @Environment(\.modelContext) var modelContext
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
     
     @State private var taskName: String = ""
     @State private var isImportant: Bool = false
@@ -27,15 +27,108 @@ struct CreateUpdateTaskView: View {
             LabeledContent {
                 //contentView
                 TextField("할 일을 입력하세요", text: $taskName)
-                    
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal, 5)
             } label: {
-                Text("해야할 일")
+                Text("TITLE")
+                    .font(.title)
+            }//: LabeledContent
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Label("IS IMPORTANT", systemImage: "exclamationmark.triangle")
+                    .font(.title3)
+                Picker("", selection: $isImportant) {
+                    Text("YES").tag(true)
+                    Text("NO").tag(false)
+                }//: Picker
+                .pickerStyle(.segmented)
+                
+                Divider()
+                
+                Toggle(isOn: $hasDueDate) {
+                    Label("HAS DUE DATE", systemImage: "calendar")
+                        .font(.title3)
+                }//: Toggle
+                if hasDueDate {
+                    DatePicker(
+                        "",
+                        selection: $dueDate,
+                        displayedComponents: [.date]
+                    )//: DatePicker
+                    .datePickerStyle(.graphical)
+                } //:CONDITION
+                
+                HStack(spacing: 20) {
+                    Button {
+                        //action
+                        if isNewTask {
+                            let newTask = TaskModel(
+                                taskName: taskName,
+                                isImportant: isImportant,
+                                dueDate: hasDueDate ? dueDate : nil
+                            )
+                            modelContext.insert(newTask)
+                        } else {
+                            task?.taskName = taskName
+                            task?.isImportant = isImportant
+                            task?.dueDate = hasDueDate ? dueDate : nil
+                        }
+                        dismiss()
+                    } label: {
+                        Text(isNewTask ? "ADD" : "UPDATE")
+                            .font(.title2.bold())
+                            .foregroundStyle(.white)
+                            .frame(height: 40)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.green.opacity(0.8))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    .disabled(taskName.isEmpty)
+                    
+                    Button {
+                        //action
+                        dismiss()
+                    } label: {
+                        Text("CANCEL")
+                            .font(.title2.bold())
+                            .foregroundStyle(.white)
+                            .frame(height: 40)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.red.opacity(0.8))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                } //:HSTACK
+            } //:VSTACK
+        }//: Form
+        .task {
+            if let task {
+                self.taskName = task.taskName
+                self.isImportant = task.isImportant
+                self.hasDueDate = task.dueDate != nil
+                self.dueDate = task.dueDate ?? Date()
             }
-
         }
-    }
+        .navigationTitle(isNewTask ? "ADD TASK" : "UPDATE TASK")
+        .navigationBarTitleDisplayMode(.inline)
+    }//:body
 }
 
-#Preview {
+#Preview("ADD TASK") {
     CreateUpdateTaskView()
+        .modelContainer(TaskModel.previewTodo)
+}
+
+#Preview("EDIT TASK") {
+    
+    let _ = TaskModel.previewTodo
+    
+    let taskModel = TaskModel(
+        taskName: "SwiftUI 여러가지 만들기",
+        isComplete: false,
+        isImportant: true,
+        dueDate: TaskModel.dateFormatter().date(from: "13년 03월 24일")
+    )
+    
+    CreateUpdateTaskView(task: taskModel)
 }
--- SwiftDataTodo/Views/MainView.swift
+++ SwiftDataTodo/Views/MainView.swift
@@ -10,8 +10,37 @@ import SwiftData
 
 struct MainView: View {
     
+    // MARK: - Properties
+    @Environment(\.modelContext) private var modelContext
+    @Query private var tasks: [TaskModel]
+    @State private var showAlert: Bool = false
+    @State private var taskTodoDelete: TaskModel?
+    @State private var isNewTask: Bool = true
+    @State private var sortOrder: SortOrder = .forward
+    @State private var filterOn: Bool = false
+    
+    // MARK: - Computed Property
+    private var sortedAndFilteredTodos: [TaskModel] {
+        let todos: [TaskModel] = filterOn ? tasks.filter { $0.isImportant } : tasks
+        return todos.sorted { sortOrder == .forward ? $0.taskName < $1.taskName : $0.taskName > $1.taskName }
+    }
+    
+    // MARK: - Body
     var body: some View {
-       
+        NavigationStack {
+            VStack {
+                if tasks.isEmpty {
+                    ContentUnavailableView(
+                        "해야 할 일 없음",
+                        systemImage: "rectangle.and.pencil.and.ellipsis",
+                        description:
+                            Text("☝️ 위의 추가 버튼을 클릭해서 해야할 일을 작성하세요.")
+                            .font(.title3)
+                    )
+                }
+            }
+            .navigationTitle(tasks.isEmpty ? "할 일 입력" : "할 일 목록")
+        } //:NAVIGATION
     }//:body
 }
 
